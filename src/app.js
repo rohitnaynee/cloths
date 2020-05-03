28d82f6b40a6fe7a577d2a87ee77f335cf9eb68c
rohit naynee <rohitnaynee@gmail.com>
2020-05-03 09:02:45 +0530

    updated with Router

--- src/app.js
+++ src/app.js
@@ -1,17 +1,31 @@
 import React from 'react';
-
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import './app.css';
+
+
+
+
+const SneakersPage = () => (
+    <div>
+      <h1> SNEAKERS PAGE </h1>
+    </div>
+);
+
+
 
 class App extends React.Component {
 
 
     render() {
         return(
-            <div>
-              <HomePage />
-            </div>
+         <div>
+          <Switch>
+            <Route  path = '/sneakers' component = {SneakersPage}    />
+            <Route exact  path = '/' component = {HomePage} />
+          </Switch>
+         </div>
         );
     }
 }
