0bbf8d9830ae0dc17579c26231939aea683388a7
rohit naynee <rohitnaynee@gmail.com>
2020-05-03 20:52:12 +0530

    done with collection-page

--- src/app.js
+++ src/app.js
@@ -2,16 +2,10 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+
 import './app.css';
-
-
-
-
-const SneakersPage = () => (
-    <div>
-      <h1> SNEAKERS PAGE </h1>
-    </div>
-);
 
 
 
@@ -22,8 +16,8 @@ class App extends React.Component {
         return(
          <div>
           <Switch>
-            <Route  path = '/sneakers' component = {SneakersPage}    />
-            <Route exact  path = '/' component = {HomePage} />
+            <Route exact  path = '/' component = { HomePage } />
+            <Route exact path = '/shop' component = { ShopPage } />
           </Switch>
          </div>
         );
