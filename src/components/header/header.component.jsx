deecd4b5444be9c37705207133afeb1ffea7bb76
rohit naynee <rohitnaynee@gmail.com>
2020-06-06 19:45:20 +0530

    added redux + &  - buttons from redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
@@ -8,6 +9,9 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+
 import './header.styles.scss';
 
 
@@ -42,8 +46,8 @@ const Header = ( { currentUser, hidden } ) => (
     </div>
 );
 
-const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
-   currentUser,
-   hidden
+const mapStateToProps = createStructuredSelector({
+   currentUser : selectCurrentUser ,
+   hidden: selectCartHidden
 });
 export default connect(mapStateToProps)(Header);
